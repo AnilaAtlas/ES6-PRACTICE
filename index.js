32de8a44cf2545adb173c84c1fd23d7e54e14d52
AnilaAtlas <anila.khan3044@gmail.com>
2025-02-16 13:26:41 +0500

    practice 1 done

---
+++ index.js
@@ -0,0 +1,8 @@
+var myObj = new Object();
+myObj.name = "Anila";
+myObj.score = 87;
+document.write(`${myObj.name} <br>`) ;
+
+var user = new Object();
+user.email = prompt("write your email address:");
+document.write(user.email);
